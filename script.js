efe599da9ec0326d70dfc812ca3e9d78e5fd6858
Noah Schwartz <noah.amir.schwartz@gmail.com>
2021-02-24 23:08:42 -0500

    minor updates

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
 //Board Module
-const gameBoard = (() =>{
+const gameBoard = (() => {
     let gameBoard = [
-                     [null, null, null],
-                     [null, null, null],
-                     [null, null, null]
-                    ];
-    const updateGameBoard = (x,y, player) => {
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
+    const updateGameBoard = (x, y, player) => {
         gameBoard[x][y] = player.getPlayerSymbol();
         console.table(gameBoard);
     }
     const getGameBoard = () => gameBoard;
-    return {getGameBoard, updateGameBoard}
+    return { getGameBoard, updateGameBoard }
 })();
 
 const pvpGame = (() => {
@@ -22,62 +22,62 @@ const pvpGame = (() => {
         players = [player("X"), player("O")];
     }
     const endTurn = () => {
-        if(turn == 0)
+        if (turn == 0)
             turn = 1;
         else turn = 0;
     }
-    const makeMove = (x,y) => {
+    const makeMove = (x, y) => {
         if (getTurn() == 0)
-            gameBoard.updateGameBoard(x,y, players[0]);
-        else gameBoard.updateGameBoard(x,y, players[1]);
+            gameBoard.updateGameBoard(x, y, players[0]);
+        else gameBoard.updateGameBoard(x, y, players[1]);
         pvpGame.endTurn();
     }
-     
+
     const getTurn = () => turn;
-    return{endTurn, startGame, getTurn, makeMove}
+    return { endTurn, startGame, getTurn, makeMove }
 })();
 
 //Player Factory Function
-const player = (player) =>{
+const player = (player) => {
     const getPlayerSymbol = () => player
-    return{getPlayerSymbol};
+    return { getPlayerSymbol };
 }
 
 
 function getNodeIndex(elm) {
     let c = elm.parentNode.children;
     for (i = 0; i < c.length; i++) if (c[i] == elm) return i;
-  }
-
-function makeMove(){
-    let index = getNodeIndex(this)
-    let grid = document.getElementsByClassName("game-board")[0];
-    let offset = Number(window.getComputedStyle(grid.children[0]).gridColumnStart) - 1; 
-    if (isNaN(offset)) {
-        offset = 0;
-      }
-      const colCount = window.getComputedStyle(grid).gridTemplateColumns.split(" ").length;
-    
-      const rowPosition = Math.floor((index + offset) / colCount);
-      const colPosition = (index + offset) % colCount;
-     
-     pvpGame.makeMove(rowPosition, colPosition);
-     this.children[0].innerText = (pvpGame.getTurn() == 0) ? "O" : "X";
 }
 
-function startGame(){
+function makeMove() {
+    let index = getNodeIndex(this)
+    let grid = document.getElementsByClassName("game-board")[0];
+    let offset = Number(window.getComputedStyle(grid.children[0]).gridColumnStart) - 1;
+    if (isNaN(offset)) {
+        offset = 0;
+    }
+    const colCount = window.getComputedStyle(grid).gridTemplateColumns.split(" ").length;
+
+    const rowPosition = Math.floor((index + offset) / colCount);
+    const colPosition = (index + offset) % colCount;
+
+    pvpGame.makeMove(rowPosition, colPosition);
+    this.children[0].innerText = (pvpGame.getTurn() == 0) ? "O" : "X";
+}
+
+function startGame() {
     addEvents()
     pvpGame.startGame();
     showBoard();
 }
 
-function showBoard(){
+function showBoard() {
     document.getElementsByClassName("game-board")[0].style.visibility = "visible"
 }
 
-function addEvents(){
+function addEvents() {
     let board = document.getElementsByClassName("game-board")[0].children
-    for (i = 0; i < board.length; i++){
+    for (i = 0; i < board.length; i++) {
         board[i].addEventListener('click', makeMove);
-       }
+    }
 }
