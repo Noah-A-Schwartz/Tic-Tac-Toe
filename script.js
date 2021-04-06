37a1959fa7b76f97992f103c0a46ff8c9cdef442
Noah Schwartz <noah.amir.schwartz@gmail.com>
2021-04-05 21:04:14 -0400

    Can now play Versus a very basiac AI
    Play versus a very basic AI, AI just picks next available space.

--- script.js
+++ script.js
@@ -12,49 +12,31 @@ const gameBoard = (() => {
     }
     const getGameBoard = () => gameBoard;
 
+    const getOpenMove = () => {
+        currentState = getGameBoard();
+        for (i = 0; i < currentState.length; i++) {
+            for (j = 0; j < currentState[i].length; j++) {
+                if (currentState[i][j] == null)
+                    return [i, j];
+            }
+        }
+    }
     const clearBoard = () => {
         gameBoard = [
             [null, null, null],
             [null, null, null],
             [null, null, null]
-        ]; 
+        ];
         let positions = document.getElementsByTagName("p");
-        for(i = 0; i < positions.length; i++){
+        for (i = 0; i < positions.length; i++) {
             positions[i].innerText = "";
         }
     }
-    return { getGameBoard, updateGameBoard, clearBoard }
-})();
 
-const pvpGame = (() => {
-    let temp = "";
-    let winner = null;
-    let turn;
-    let players;
-    const startGame = () => {
-        winner = null;
-        turn = 0;
-        players = [player("X"), player("O")];
-    }
-    const endTurn = () => {
-        if (turn == 0)
-            turn = 1;
-        else turn = 0;
-        isWinner();
-    }
-    const makeMove = (x, y) => {
-        if (getTurn() == 0)
-            gameBoard.updateGameBoard(x, y, players[0]);
-        else gameBoard.updateGameBoard(x, y, players[1]);
-
-        pvpGame.endTurn();
-    }
-    
-    //000
-    const isWinner = () => {
-        
+    const isWinner = (players) => {
         temp = "";
-        let currentState = gameBoard.getGameBoard();
+        winner = null;
+        let currentState = getGameBoard();
         //Rows
         for (i = 0; i < currentState.length; i++) {
             if (winner == null)
@@ -80,7 +62,7 @@ const pvpGame = (() => {
         }
 
         //Colums
-        
+
         temp = "";
         for (i = 0; i < currentState.length; i++) {
             if (winner == null)
@@ -105,7 +87,7 @@ const pvpGame = (() => {
 
         }
         //Diagonal From Top left
-        
+
         temp = "";
         for (i = 0; i < currentState.length; i++) {
             temp += currentState[i][i];
@@ -123,7 +105,7 @@ const pvpGame = (() => {
             displayDiagonalFromLeft()
             displayWinningPlayer(winner.getPlayerSymbol());
         }
-        
+
         temp = "";
         lastRow = null;
         lastColumn = null;
@@ -138,23 +120,71 @@ const pvpGame = (() => {
             console.log("Player " + winner.getPlayerSymbol() + " is the Winner");
             displayDiagonalFromRight()
             displayWinningPlayer(winner.getPlayerSymbol());
-            
+
         }
         else if (temp == "XXX") {
             winner = players[0];
             console.log("Player " + winner.getPlayerSymbol() + " is the Winner");
             displayDiagonalFromRight()
             displayWinningPlayer(winner.getPlayerSymbol());
-            
+
         }
-        if(winner != null){
+        if (winner != null) {
             disableEvents();
         }
+    }
+    return { getGameBoard, updateGameBoard, clearBoard, isWinner, getOpenMove }
+})();
+
+
+const game = (() => {
+    let turn;
+    let players;
+    let aiGame;
+    const startGame = (gameType) => {
+        winner = null;
+        turn = 0;
+        players = [player("X"), player("O")];
+        if (gameType == 1)
+            aiGame = true;
+        else aiGame = false;
+
+    }
+    const endTurn = () => {
+        if (turn == 0)
+            turn = 1;
+        else turn = 0;
+        gameBoard.isWinner(players);
+    }
+
+    const makeAiMove = () => {
+        let firstOpenMove = gameBoard.getOpenMove()
+        console.table(firstOpenMove);
+        gameBoard.updateGameBoard(firstOpenMove[0], firstOpenMove[1], players[1]);
+        showAiMove(firstOpenMove[0], firstOpenMove[1]);
+
+    }
+
+    const makeMove = (x, y) => {
+        if (getTurn() == 0) {
+            gameBoard.updateGameBoard(x, y, players[0]);
+            endTurn()
+        }
+        else if (getTurn() == 1 && aiGame == false) {
+            gameBoard.updateGameBoard(x, y, players[1]);
+            endTurn()
+        }
+
+        if (aiGame == true) {
+            makeAiMove()
+            endTurn();
+        }
+
     }
 
 
     const getTurn = () => turn;
-    return { endTurn, startGame, getTurn, makeMove }
+    return { endTurn, startGame, getTurn, makeMove, makeAiMove }
 })();
 
 //Player Factory Function
@@ -180,18 +210,32 @@ function makeMove() {
 
     const rowPosition = Math.floor((index + offset) / colCount);
     const colPosition = (index + offset) % colCount;
-
-    pvpGame.makeMove(rowPosition, colPosition);
-    this.children[0].innerText = (pvpGame.getTurn() == 0) ? "O" : "X";
+    console.log("TURN: " + game.getTurn());
+    this.children[0].innerText = (game.getTurn() == 0) ? "X" : "O";
+    game.makeMove(rowPosition, colPosition);
     this.removeEventListener("click", makeMove);
+}
+
+function showAiMove(x, y) {
+    let grid = document.getElementsByClassName("slot");
+    if (x == 0)
+        y += 0;
+    else if (x == 1)
+        y += 3;
+    else
+        y += 6;
+    console.log("TURN: " + game.getTurn());
+    grid[y].innerText = "O";
 }
 
 function startGame(button) {
     addEvents();
-    pvpGame.startGame();
+    if (button.id == "PvPButton")
+        game.startGame(0);
+    else game.startGame(1);
     showBoard();
     document.getElementsByClassName("game-options")[0].style.display = "none";
-    if(document.getElementsByTagName("h2")[0].style.display != "none"){
+    if (document.getElementsByTagName("h2")[0].style.display != "none") {
         document.getElementsByTagName("h2")[0].style.display = "none";
     }
 }
@@ -200,7 +244,7 @@ function startGame(button) {
 function showBoard() {
     gameBoard.clearBoard();
     document.getElementsByClassName("game-board")[0].style.visibility = "visible"
-    
+
 }
 
 function addEvents() {
@@ -210,14 +254,14 @@ function addEvents() {
     }
 }
 
-function disableEvents(){
+function disableEvents() {
     let board = document.getElementsByClassName("game-board")[0].children
     for (i = 0; i < board.length; i++) {
         board[i].removeEventListener('click', makeMove);
     }
 }
 
-function displayWinningPlayer(winner){
+function displayWinningPlayer(winner) {
     let winningText = document.getElementsByTagName("h2")[0];
     winningText.textContent = "Winner is " + winner;
     winningText.style.display = "block";
@@ -261,13 +305,13 @@ function displayWinningColumn(column) {
     }
 }
 
-function displayDiagonalFromLeft(){
+function displayDiagonalFromLeft() {
     grid.children[0].children[0].style.color = "red";
     grid.children[4].children[0].style.color = "red";
     grid.children[8].children[0].style.color = "red";
 }
 
-function displayDiagonalFromRight(){
+function displayDiagonalFromRight() {
     grid.children[2].children[0].style.color = "red";
     grid.children[4].children[0].style.color = "red";
     grid.children[6].children[0].style.color = "red";
